c6539b602cdfad09cd8edc908550ebe4f209209c
nasir aisha <nasircutieeesha@gmail.com>
2023-04-27 13:08:35 -0700

    adding task file

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""contains the concat function"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """joins two strings
+
+        Args:
+            str1(str): first string
+            str2(str): second string
+
+        Return: a string that is the made of both strings
+    """
+    return str1 + str2
