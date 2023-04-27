b4344c0b83f602e5600c291e1c5fbed0f749e508
nasir aisha <nasircutieeesha@gmail.com>
2023-04-27 13:10:33 -0700

    adding task file

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""contains the to_str method"""
+
+
+def to_str(n: float) -> str:
+    """converts a floating number to str
+        Args:
+            n(float): any floating number
+
+        Return: a string representation of the n param"""
+    return str(n)
