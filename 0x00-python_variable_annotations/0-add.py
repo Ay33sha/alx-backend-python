d838a59c3484280b0273466ee0f411a5198985bf
nasir aisha <nasircutieeesha@gmail.com>
2023-04-27 13:07:11 -0700

    adding task file

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Contains a simple add function"""
+
+
+def add(a: float, b: float) -> float:
+    """ Simple add function
+
+        Args:
+            a(float): first arg
+            b(float): second arg
+
+        Return: the sum of a and b
+    """
+    return a + b
