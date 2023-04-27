4c15eb4fe58cb4fd03bcd05c570142f8b9855f80
nasir aisha <nasircutieeesha@gmail.com>
2023-04-27 13:09:40 -0700

    adding task file

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""contains the floor function"""
+
+
+def floor(n: float) -> int:
+    """floors the number passed
+
+        Args:
+            n(float): any floating number
+
+        Return: the floor version of the number
+    """
+    return int(n)
