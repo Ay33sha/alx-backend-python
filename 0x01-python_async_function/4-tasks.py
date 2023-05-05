9724921d5bfb4e407419678fcc4db0f32ee30d59
nasir aisha <nasircutieeesha@gmail.com>
2023-05-05 16:31:46 -0700

    adding task file

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,22 +1,17 @@
 #!/usr/bin/env python3
-"""contains the function/coroutine task_wait_n"""
+'''Task 4's module.
+'''
 import asyncio
-from typing import TYPE_CHECKING, List
+from typing import List
+
+
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """spawns task_wait_random n times with the specified max_delay
-
-        Args:
-            n(int): number of times wait_randoms should be spawned
-            max_delay(int): max_delay of wait_random
-
-        Return: the list of all delays(float values) ina ascending
-            order with sort()
-    """
-    # lst = [await wait_random(max_delay) for i in range(n)]
-    lst = await asyncio.gather(
-            *(task_wait_random(max_delay) for i in range(n))
-            )
-    return sorted(lst)
+    '''Executes task_wait_random n times.
+    '''
+    wait_times = await asyncio.gather(
+        *tuple(map(lambda _: task_wait_random(max_delay), range(n)))
+    )
+    return sorted(wait_times)
